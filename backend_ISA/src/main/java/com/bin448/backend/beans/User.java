6879e30976f469d451e5bc9f65d3aa63be446eca
Ivan <ivanindjic@gmail.com>
2018-11-17 17:15:20 +0100

    polja

--- backend_ISA/src/main/java/com/bin448/backend/beans/User.java
+++ backend_ISA/src/main/java/com/bin448/backend/beans/User.java
@@ -1,4 +1,59 @@
 package com.bin448.backend.beans;
 
 public class User {
+    private String nema;
+    private String lastName;
+    private String email;
+    private String username;
+    private String city;
+    private String telephone;
+
+
+    public String getNema() {
+        return nema;
+    }
+
+    public void setNema(String nema) {
+        this.nema = nema;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }
