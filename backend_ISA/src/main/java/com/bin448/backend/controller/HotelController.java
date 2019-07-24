4be358a0adfc13531866c79a287de08bdf466d8a
NikolinaTomic <tomic.ninja@gmail.com>
2019-07-24 16:46:25 +0200

    hotel converter and hotel dto

--- backend_ISA/src/main/java/com/bin448/backend/controller/HotelController.java
+++ backend_ISA/src/main/java/com/bin448/backend/controller/HotelController.java
@@ -1,4 +1,28 @@
 package com.bin448.backend.controller;
 
+import com.bin448.backend.entity.DTOentity.HotelDTO;
+import com.bin448.backend.service.HotelService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/hotel")
 public class HotelController {
+
+    private final HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addHotel(@RequestBody @Valid HotelDTO hotelDTO) {
+        hotelService.addHotel(hotelDTO);
+        return ResponseEntity.ok(String.format("Hotel %s is added", hotelDTO.getName()));
+    }
 }
