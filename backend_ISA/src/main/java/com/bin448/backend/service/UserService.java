adebfba483fae4f1c00a02e629f8c4fb60ce72e2
papulanovic96 <b.papulanovic@gmail.com>
2019-07-25 19:14:56 +0200

    user converter and controller

--- backend_ISA/src/main/java/com/bin448/backend/service/UserService.java
+++ backend_ISA/src/main/java/com/bin448/backend/service/UserService.java
@@ -1,4 +1,12 @@
 package com.bin448.backend.service;
 
+import com.bin448.backend.entity.DTOentity.UserDTO;
+import com.bin448.backend.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface UserService {
+    boolean modify(UserDTO oldUser);
+    void deleteFriend(UserDTO friend);
+    User addFriend(UserDTO friend);
 }
